240f4757aad2134510f598161c9bd8b954978ba4
Lucas Riccombene <lriccombene@gmail.com>
2020-05-27 18:59:58 -0300

    se actualiza firts-start.sh con un install compos

--- bin/firts-start.sh
+++ bin/firts-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker-compose exec app composer update
-docker-composer exec app chmod 777 ./web/assets -R
-docker-composer exec app chmod 777 ./runtime -R
+docker-compose exec app composer install
+docker-compose exec app chmod 777 ./web/assets -R
+docker-compose exec app chmod 777 ./runtime -R
