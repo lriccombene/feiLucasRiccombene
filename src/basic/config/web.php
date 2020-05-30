93532e90ca5cf8e6fca085c5e4a74f562678fcfd
Lucas Riccombene <lriccombene@gmail.com>
2020-05-30 12:08:19 -0300

    se configura para que yii interprete los json enviados desde http

--- src/basic/config/web.php
+++ src/basic/config/web.php
@@ -15,6 +15,9 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'zmSgebsjBJucynP-Zq5wbIMgUdO10M5o',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
