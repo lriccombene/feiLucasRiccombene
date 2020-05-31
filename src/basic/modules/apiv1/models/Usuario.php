9bf3203e255082465a5588a4c2909e456a22ca1b
Lucas Riccombene <lriccombene@gmail.com>
2020-05-31 12:28:34 -0300

    se crea una action en el controler site y se crea una vista para esa action Vue

--- src/basic/modules/apiv1/models/Usuario.php
+++ src/basic/modules/apiv1/models/Usuario.php
@@ -4,10 +4,14 @@ namespace app\modules\apiv1\models;
 
 class Usuario extends \app\models\Usuario
 {
+    private $valor;
+
     public function fields (){
         return ['id', 'nombre','apellido'];
     }
     public function extraFields(){
         return ['permiso' ]; 
     }
+  
 }
+
---
+++ src/basic/views/site/vue.php
@@ -0,0 +1,22 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Vue';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+
+
+<div class="site-vue">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        This is the Vue page.
+    </p>
+
+    <code><?= __FILE__ ?></code>
+</div>
